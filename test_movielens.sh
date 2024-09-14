544b0dff8f9aa0281b01793b0ac88fd9a6ce34b1
leon.kepler <leon.kepler@bytedance.com>
2024-09-14 17:46:18 +0800

    new

--- test_movielens.sh
+++ test_movielens.sh
@@ -1,14 +1,14 @@
-CUDA_VISIBLE_DEVICES=2 python3 main.py \
+CUDA_VISIBLE_DEVICES=0 python3 main.py \
 --mode test \
 --batch_size 4 \
 --accumulate_grad_batches 32 \
 --dataset movielens_data \
---data_dir /mnt/bn/data-tns-live-llm/leon/LLaRA-similar_seq_as_demo-/data/LLaRA/movielens \
+--data_dir /mnt/bn/data-tns-live-llm/leon/recom/LLaRA-similar_seq_as_demo-/data/LLaRA/movielens \
 --cans_num 20 \
---prompt_path /mnt/bn/data-tns-live-llm/leon/LLaRA-similar_seq_as_demo-/prompt/movie.txt \
+--prompt_path /mnt/bn/data-tns-live-llm/leon/recom/LLaRA-similar_seq_as_demo-/prompt/movie.txt \
 --rec_embed SASRec \
 --llm_tuning lora \
---llm_path /mnt/bn/data-tns-live-llm/leon/datasets/rec/movielens_unsloth \
+--llm_path /mnt/bn/data-tns-live-llm/leon/datasets/rec/score_model_adapter \
 --rec_model_path ./rec_model/movielens.pt \
 --output_dir ./output/movielens/ \
 --log_dir movielens_logs \
